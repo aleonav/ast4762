b2fd4628e784bf72b27120f8d07d29bfe0cd6de7
aleonav <a.murch.leo@gmail.com>
2026-09-15 18:06:44 -0400

    Final commit for prob2(g)
    finished square, tidied up the support function, and and commiting to git.

--- homework/hw3_aleonav/hw3_aleonav_support_functions.py
+++ homework/hw3_aleonav/hw3_aleonav_support_functions.py
@@ -1,12 +1,6 @@
-"""This is the docstring for the example.py module.  Modules should
-have short, all-lowercase names.  The module name may have underscores if
-this improves readability.
-
-Every module should have a docstring at the very top of the file.  The
-module's docstring may extend over multiple lines.  If your docstring does
-extend over multiple lines, the closing three quotation marks must be on
-a line by themselves, preferably preceeded by a blank line.
-
+"""Returns the square of the inputted variable that is type of
+array_like, scalar (int, float, double, etc)
+by using python's built in [x]**2 opperator.
 """
 
 import os 
@@ -25,115 +19,44 @@ def square(var1) :
 
     Parameters
     ----------
-    var1 : array_like / scalar
-        Array_like means all those objects -- lists, nested lists, etc. --
-        that can be converted to an array.  We can also refer to
-        variables like `var1`.
+    var1 : array_like / scalar (int, float, double, etc)
 
     Returns
     -------
-    describe : type
-        Explanation
-    output : type
-        Explanation
-    tuple : type
-        Explanation
-    items : type
-        even more explaining
+    output : array_like / scalar (int, float, double, etc)
+        var1**2 of the input, var1. Matches its initial variable type.
 
-    Other Parameters
-    ----------------
-    only_seldom_used_keywords : type
-        Explanation
-    common_parameters_listed_above : type
-        Explanation
-
-    Raises
-    ------
-    BadException
-        Because you shouldn't have done that.
 
     See Also
     --------
-    otherfunc : relationship (optional)
-    newfunc : Relationship (optional), which could be fairly long, in which
-              case the line wraps here.
-    thirdfunc, fourthfunc, fifthfunc
+    ** : built in python square notation.
+    np.square() : Numpy's square function
+    math.pow(): math's power function, for math.pow(var1, 2)
 
     Notes
     -----
-    Notes about the implementation algorithm (if needed).
-
-    This can have multiple paragraphs.
-
-    You may include some math:
-
-    .. math:: X(e^{j\omega } ) = x(n)e^{ - j\omega n}
-
-    And even use a greek symbol like :math:`omega` inline.
-
-    References
-    ----------
-    Cite the relevant literature, e.g. [1]_.  You may also cite these
-    references in the notes section above.
-
-    .. [1] O. McNoleg, "The integration of GIS, remote sensing,
-       expert systems and adaptive co-kriging for environmental habitat
-       modelling of the Highland Haggis using object-oriented, fuzzy-logic
-       and neural-network techniques," Computers & Geosciences, vol. 22,
-       pp. 585-588, 1996.
+    Uses the simple built in python function to return var1**2, the square value of var1.
 
     Examples
     --------
-    These are written in doctest format, and should illustrate how to
-    use the function.  Use the plain python prompt (">>> ") for the
-    example commands, and put the expected output on a line without a
-    prompt, as follows:
+    >>> var1 = np.array([1, 2, 3, 4])
+    >>> var1_sq = square(var1)
+    >>> print(var1_sq)
+    [1, 4, 9, 16]
 
+    >>> var2 = 3
+    >>> var2_sq = square(var2)
+    >>> print(var2_sq)
+    9
 
-    >>> a=[1,2,3]
-    >>> print([x + 3 for x in a])
-    [4, 5, 6]
-    >>> print("a\n\nb")
-    a
-    <BLANKLINE>
-    b
-
-    The examples in docstrings can be run with the 'doctest' package:
-
-    python -m doctest -v doctest_simple_with_docs.py
-
-    See:
-
-    https://pymotw.com/2/doctest/
-
-    for more examples.  There are several different testing packages,
-    including 'doctest' and 'nose'.  All use the same format when it
-    comes to tests in docstrings, but offer different testing
-    functionality.
-
-    nosetests --with-doctest your_python_file.py
-
-    If your example prints random numbers, use an ellipsis ('...') to
-    skip the part of the output that is variable.
+    >>> var3 = 3.3
+    >>> var3_sq = square(var3)
+    >>> print(var3_sq)
+    10.889999999999999
 
     Revisions
     ---------
-    2008-09-11 jh@physics.ucf.edu added Revisions section
-               The Revisions section is where you record modifications
-               to the file.  In the Python and NumPy sources, this is
-               handled by a revision control system like Git.  If you
-               are not using one, and even sometimes if you are, it's
-               good to put the revision notes right into the
-               docstring.
-
-    2008-09-11 jh@physics.ucf.edu downloaded and edited from
-        http://projects.scipy.org/scipy/numpy/browser/trunk/doc/example.py
-    2014-08-26 jh@physics.ucf.edu updated docstring
-    2016-09-14 jh@physics.ucf.edu Added doctest commands and discussion.
-    2018-08-30 jh@physics.ucf.edu updated original from
-    https://github.com/andyk/load_gen/blob/master/numpy/doc/example.py
-        and merged local changes.
+    2026-09-15: Created square in it's current form
     """
 
     #square and return var1, no pass needed since it does something.
